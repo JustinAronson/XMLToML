850f050655156dccd85aecf321f22361b2da2e0e
JustinAronson <JustinAronson@users.noreply.github.com>
2020-07-15 15:17:45 -0400

    Updated normaliser
    Fixed additional spaces being added between characters and improved efficiency

--- csvNormaliser.py
+++ csvNormaliser.py
@@ -20,7 +20,10 @@ if __name__ == '__main__':
     columnShouldTransfer = []
     
     #Min number of values in a column for it to transfer
-    minColumnEntries = 0
+    minColumnEntries = 50000
+    
+    #List of column headers that meet the minimum entry threshold
+    columnHeaderListMeetingMin = []
     
     #Index of Max column that makes the minColumnEntries threshold
     maxColumnsToCheck = 0
@@ -34,33 +37,42 @@ if __name__ == '__main__':
         columnHeaderSplitter.whitespace_split = True
         columnHeaderList = list(columnHeaderSplitter)
         numberOfColumns = len(columnHeaderList)    
-        columnHeaderList[numberOfColumns-1] = columnHeaderList[numberOfColumns-1].rstrip()    
+        columnHeaderList[numberOfColumns-1] = columnHeaderList[numberOfColumns-1].rstrip()
     
     with open('e:\\columnCount.csv') as columnCountCSV:
         csv_reader = csv.reader(columnCountCSV, delimiter=',')
         for csvRow in csv_reader:
             columnCounts[csvRow[0]] = int(csvRow[1])
             
+    
     for i in range(numberOfColumns):
         if columnCounts[columnHeaderList[i]] >= minColumnEntries:
             maxColumnsToCheck = i
             columnShouldTransfer.append(True)
+            columnHeaderListMeetingMin.append(columnHeaderList[i])
         else:
             columnShouldTransfer.append(False)
             
-        
-            
+    columnHeaderListMeetingMin.append("rowLength")
+    numberOfColumns += 1
         
     with open('e:\\csvFile.csv', encoding="utf-8") as csvFile:
         
         writeFile = open(r"E:\\csvFileWithHeaders.csv", 'w+', newline="", encoding = "utf-8")
         writer = csv.writer(writeFile)
-        writer.writerows([columnHeaderList,])
+        writer.writerows([columnHeaderListMeetingMin,])
                 
         csv_reader = csv.reader(csvFile, delimiter=',')
         lCount = 0
         
+        rowLength = -1
+        
         for csvRow in csv_reader:
+            
+            rowLength = len(csvRow)
+            
+            while len(csvRow) <= numberOfColumns:
+                csvRow.append('')
             
             newRow = []
             
@@ -68,13 +80,15 @@ if __name__ == '__main__':
                 if i < len(csvRow):
                     if columnShouldTransfer[i]:
                         newRow.append(csvRow[i])
-                    else:
-                        newRow.append('')
-            writer.writerows([newRow,])
-            if lCount % 1000 == 0:
-                print(str(lCount) + ': ' + str(datetime.datetime.now()))
+            
+            newRow.append(rowLength)
+            
+            writer.writerow(newRow)
 
             lCount += 1
+
+            if lCount % 1000 == 0:
+                print(str(lCount) + ': ' + str(datetime.datetime.now()))
 
     
         writeFile.close()
